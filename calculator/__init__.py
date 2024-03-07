453513e588b42edd5bce741a84b1c3f0ceb7cfcb
imchugh <imchugh@unimelb.edu.au>
2024-03-08 10:41:54 +1100

    Add project code and documentation

---
+++ calculator/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# calculator/__init__.py
+
+"""Do math with your own functions.
+
+Modules exported by this package:
+
+- `calculations`: Provide several sample math calculations.
+"""
