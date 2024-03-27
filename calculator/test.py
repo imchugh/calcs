d95f6b2fa10b76f45f3d14dbe51b9ed360fa1826
imchugh <imchugh@unimelb.edu.au>
2024-03-27 11:28:28 +1100

    Added new file test.py for testing

---
+++ calculator/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 27 11:21:13 2024
+
+@author: imchugh
+"""
+
+"""Provide several sample dog calculations.
+
+Blah de blah de blah"""
